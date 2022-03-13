51fb7a5366dc90f79a4b7ac98801c3e3252eecb9
ilyaizvekov <ilya.izvekov_dev@lenta.ru>
2022-03-13 21:57:49 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,5 +9,7 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = item.getCreated().format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+        Item item1 = new Item();
+        System.out.println(item1);
     }
 }
