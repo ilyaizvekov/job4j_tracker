7b565ea8f8c66a45bb53a6fd4b0d7982cc5b14a6
ilyaizvekov <ilya.izvekov_dev@lenta.ru>
2022-04-07 21:00:10 +0300

    2. final [#334073 #284073]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -1,9 +1,28 @@
 package ru.job4j.ooa;
 
+import java.util.Objects;
+
 public final class Airbus extends Aircraft {
     private static final int COUNT_ENGINE = 2;
 
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Airbus airbus = (Airbus) o;
+        return Objects.equals(name, airbus.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 
     public Airbus(String name) {
         this.name = name;
@@ -23,7 +42,7 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        int engine = name == "A380" ? 4 : COUNT_ENGINE;
+        int engine = "A380".equals(name) ? 4 : COUNT_ENGINE;
         System.out.println("Количество двигателей равно: " + engine);
     }
 
