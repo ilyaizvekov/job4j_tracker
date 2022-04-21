8dfd00d07df5de4dc8568994ea36495440bc89eb
ilyaizvekov <ilya.izvekov_dev@lenta.ru>
2022-04-21 15:00:53 +0300

    3. Банковские переводы [#10038 #291533]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -9,9 +9,7 @@ public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-        if (!users.containsKey(user)) {
-            users.put(user, new ArrayList<Account>());
-        }
+            users.putIfAbsent(user, new ArrayList<>());
     }
 
     public void addAccount(String passport, Account account) {
